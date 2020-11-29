e3dc3837d1aa202e9355c63a816dcf8d9a5319ff
patryksi11 <patryksi111@gmail.com>
2020-11-29 23:28:02 +0100

    lut

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -74,8 +74,8 @@ void Error_Handler(void);
 #define PDM_OUT_GPIO_Port GPIOC
 #define B1_Pin GPIO_PIN_0
 #define B1_GPIO_Port GPIOA
-#define I2S3_WS_Pin GPIO_PIN_4
-#define I2S3_WS_GPIO_Port GPIOA
+#define DMA_Pin GPIO_PIN_2
+#define DMA_GPIO_Port GPIOA
 #define SPI1_SCK_Pin GPIO_PIN_5
 #define SPI1_SCK_GPIO_Port GPIOA
 #define SPI1_MISO_Pin GPIO_PIN_6
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
+/*
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
@@ -16,10 +16,10 @@
   *
   ******************************************************************************
   */
+
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "usb_host.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -44,11 +44,11 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
-I2C_HandleTypeDef hi2c1;
-
-I2S_HandleTypeDef hi2s3;
+DAC_HandleTypeDef hdac;
 
 SPI_HandleTypeDef hspi1;
+
+TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN PV */
 
@@ -58,23 +58,183 @@ SPI_HandleTypeDef hspi1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
-static void MX_I2C1_Init(void);
-static void MX_I2S3_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_ADC1_Init(void);
-void MX_USB_HOST_Process(void);
-
+static void MX_TIM3_Init(void);
+static void MX_DAC_Init(void);
 /* USER CODE BEGIN PFP */
 
+#define DDS_BUF_LEN 1024
+const uint16_t ddsBuf[DDS_BUF_LEN]=
+{
+		2048,2060,2073,2085,2098,2110,2123,2135,
+		2148,2161,2173,2186,2198,2211,2223,2236,
+		2248,2261,2273,2286,2298,2311,2323,2335,
+		2348,2360,2373,2385,2398,2410,2422,2435,
+		2447,2459,2472,2484,2496,2508,2521,2533,
+		2545,2557,2569,2581,2594,2606,2618,2630,
+		2642,2654,2666,2678,2690,2702,2714,2725,
+		2737,2749,2761,2773,2784,2796,2808,2819,
+		2831,2843,2854,2866,2877,2889,2900,2912,
+		2923,2934,2946,2957,2968,2979,2990,3002,
+		3013,3024,3035,3046,3057,3068,3078,3089,
+		3100,3111,3122,3132,3143,3154,3164,3175,
+		3185,3195,3206,3216,3226,3237,3247,3257,
+		3267,3277,3287,3297,3307,3317,3327,3337,
+		3346,3356,3366,3375,3385,3394,3404,3413,
+		3423,3432,3441,3450,3459,3468,3477,3486,
+		3495,3504,3513,3522,3530,3539,3548,3556,
+		3565,3573,3581,3590,3598,3606,3614,3622,
+		3630,3638,3646,3654,3662,3669,3677,3685,
+		3692,3700,3707,3714,3722,3729,3736,3743,
+		3750,3757,3764,3771,3777,3784,3791,3797,
+		3804,3810,3816,3823,3829,3835,3841,3847,
+		3853,3859,3865,3871,3876,3882,3888,3893,
+		3898,3904,3909,3914,3919,3924,3929,3934,
+		3939,3944,3949,3953,3958,3962,3967,3971,
+		3975,3980,3984,3988,3992,3996,3999,4003,
+		4007,4010,4014,4017,4021,4024,4027,4031,
+		4034,4037,4040,4042,4045,4048,4051,4053,
+		4056,4058,4060,4063,4065,4067,4069,4071,
+		4073,4075,4076,4078,4080,4081,4083,4084,
+		4085,4086,4087,4088,4089,4090,4091,4092,
+		4093,4093,4094,4094,4094,4095,4095,4095,
+		4095,4095,4095,4095,4094,4094,4094,4093,
+		4093,4092,4091,4090,4089,4088,4087,4086,
+		4085,4084,4083,4081,4080,4078,4076,4075,
+		4073,4071,4069,4067,4065,4063,4060,4058,
+		4056,4053,4051,4048,4045,4042,4040,4037,
+		4034,4031,4027,4024,4021,4017,4014,4010,
+		4007,4003,3999,3996,3992,3988,3984,3980,
+		3975,3971,3967,3962,3958,3953,3949,3944,
+		3939,3934,3929,3924,3919,3914,3909,3904,
+		3898,3893,3888,3882,3876,3871,3865,3859,
+		3853,3847,3841,3835,3829,3823,3816,3810,
+		3804,3797,3791,3784,3777,3771,3764,3757,
+		3750,3743,3736,3729,3722,3714,3707,3700,
+		3692,3685,3677,3669,3662,3654,3646,3638,
+		3630,3622,3614,3606,3598,3590,3581,3573,
+		3565,3556,3548,3539,3530,3522,3513,3504,
+		3495,3486,3477,3468,3459,3450,3441,3432,
+		3423,3413,3404,3394,3385,3375,3366,3356,
+		3346,3337,3327,3317,3307,3297,3287,3277,
+		3267,3257,3247,3237,3226,3216,3206,3195,
+		3185,3175,3164,3154,3143,3132,3122,3111,
+		3100,3089,3078,3068,3057,3046,3035,3024,
+		3013,3002,2990,2979,2968,2957,2946,2934,
+		2923,2912,2900,2889,2877,2866,2854,2843,
+		2831,2819,2808,2796,2784,2773,2761,2749,
+		2737,2725,2714,2702,2690,2678,2666,2654,
+		2642,2630,2618,2606,2594,2581,2569,2557,
+		2545,2533,2521,2508,2496,2484,2472,2459,
+		2447,2435,2422,2410,2398,2385,2373,2360,
+		2348,2335,2323,2311,2298,2286,2273,2261,
+		2248,2236,2223,2211,2198,2186,2173,2161,
+		2148,2135,2123,2110,2098,2085,2073,2060,
+		2048,2035,2022,2010,1997,1985,1972,1960,
+		1947,1934,1922,1909,1897,1884,1872,1859,
+		1847,1834,1822,1809,1797,1784,1772,1760,
+		1747,1735,1722,1710,1697,1685,1673,1660,
+		1648,1636,1623,1611,1599,1587,1574,1562,
+		1550,1538,1526,1514,1501,1489,1477,1465,
+		1453,1441,1429,1417,1405,1393,1381,1370,
+		1358,1346,1334,1322,1311,1299,1287,1276,
+		1264,1252,1241,1229,1218,1206,1195,1183,
+		1172,1161,1149,1138,1127,1116,1105,1093,
+		1082,1071,1060,1049,1038,1027,1017,1006,
+		995,984,973,963,952,941,931,920,
+		910,900,889,879,869,858,848,838,
+		828,818,808,798,788,778,768,758,
+		749,739,729,720,710,701,691,682,
+		672,663,654,645,636,627,618,609,
+		600,591,582,573,565,556,547,539,
+		530,522,514,505,497,489,481,473,
+		465,457,449,441,433,426,418,410,
+		403,395,388,381,373,366,359,352,
+		345,338,331,324,318,311,304,298,
+		291,285,279,272,266,260,254,248,
+		242,236,230,224,219,213,207,202,
+		197,191,186,181,176,171,166,161,
+		156,151,146,142,137,133,128,124,
+		120,115,111,107,103,99,96,92,
+		88,85,81,78,74,71,68,64,
+		61,58,55,53,50,47,44,42,
+		39,37,35,32,30,28,26,24,
+		22,20,19,17,15,14,12,11,
+		10,9,8,7,6,5,4,3,
+		2,2,1,1,1,0,0,0,
+		0,0,0,0,1,1,1,2,
+		2,3,4,5,6,7,8,9,
+		10,11,12,14,15,17,19,20,
+		22,24,26,28,30,32,35,37,
+		39,42,44,47,50,53,55,58,
+		61,64,68,71,74,78,81,85,
+		88,92,96,99,103,107,111,115,
+		120,124,128,133,137,142,146,151,
+		156,161,166,171,176,181,186,191,
+		197,202,207,213,219,224,230,236,
+		242,248,254,260,266,272,279,285,
+		291,298,304,311,318,324,331,338,
+		345,352,359,366,373,381,388,395,
+		403,410,418,426,433,441,449,457,
+		465,473,481,489,497,505,514,522,
+		530,539,547,556,565,573,582,591,
+		600,609,618,627,636,645,654,663,
+		672,682,691,701,710,720,729,739,
+		749,758,768,778,788,798,808,818,
+		828,838,848,858,869,879,889,900,
+		910,920,931,941,952,963,973,984,
+		995,1006,1017,1027,1038,1049,1060,1071,
+		1082,1093,1105,1116,1127,1138,1149,1161,
+		1172,1183,1195,1206,1218,1229,1241,1252,
+		1264,1276,1287,1299,1311,1322,1334,1346,
+		1358,1370,1381,1393,1405,1417,1429,1441,
+		1453,1465,1477,1489,1501,1514,1526,1538,
+		1550,1562,1574,1587,1599,1611,1623,1636,
+		1648,1660,1673,1685,1697,1710,1722,1735,
+		1747,1760,1772,1784,1797,1809,1822,1834,
+		1847,1859,1872,1884,1897,1909,1922,1934,
+		1947,1960,1972,1985,1997,2010,2022,2035,
+};
 
-#define ADC_BUFFER_LEN 512
+
+
+#define ADC_BUFFER_LEN 16
   uint16_t adcBuffer[ADC_BUFFER_LEN];
+  uint16_t adcOVS = 0;
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+  void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
 
+
+//  	for (int i=0; i<ADC_BUFFER_LEN;i++){
+
+ // 	}
+  	HAL_GPIO_TogglePin(DMA_GPIO_Port, DMA_Pin);
+
+/*
+  		for(int i=0; i<ADC_BUFFER_LEN; i++){
+  			adcOVS += adcBuffer[1];
+  		}
+
+
+
+  		adcOVS /= ADC_BUFFER_LEN;
+  		HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, adcOVS);
+*/
+
+  	static uint16_t sample;
+  	if(sample>=DDS_BUF_LEN)sample=0;
+  	HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, ddsBuf[sample]);
+  	sample++;
+
+
+  	//ddsBuf[DDS_BUF_LEN]
+  //HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_L, adcOvs);
+  }
 /* USER CODE END 0 */
 
 /**
@@ -106,26 +266,32 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_I2S3_Init();
   MX_SPI1_Init();
-  MX_USB_HOST_Init();
   MX_ADC1_Init();
+  MX_TIM3_Init();
+  MX_DAC_Init();
   /* USER CODE BEGIN 2 */
+
+
+
+
+ // void HAL_ADC_ConvCpltCall
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
+  HAL_TIM_Base_Start(&htim3);
   HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adcBuffer,ADC_BUFFER_LEN);
+  HAL_DAC_Start(&hdac,DAC_CHANNEL_1);
 
   while (1)
   {
     /* USER CODE END WHILE */
-    //MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
+	  //HAL_GPIO_TogglePin(DMA_GPIO_Port, DMA_Pin);
+	  HAL_Delay(20);
   }
   /* USER CODE END 3 */
 }
@@ -138,7 +304,6 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
   */
@@ -172,13 +337,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
-  PeriphClkInitStruct.PLLI2S.PLLI2SN = 192;
-  PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
 }
 
 /**
@@ -203,14 +361,14 @@ static void MX_ADC1_Init(void)
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ENABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T3_TRGO;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
@@ -232,70 +390,40 @@ static void MX_ADC1_Init(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
+  * @brief DAC Initialization Function
   * @param None
   * @retval None
   */
-static void MX_I2C1_Init(void)
+static void MX_DAC_Init(void)
 {
 
-  /* USER CODE BEGIN I2C1_Init 0 */
+  /* USER CODE BEGIN DAC_Init 0 */
 
-  /* USER CODE END I2C1_Init 0 */
+  /* USER CODE END DAC_Init 0 */
 
-  /* USER CODE BEGIN I2C1_Init 1 */
+  DAC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
+  /* USER CODE BEGIN DAC_Init 1 */
 
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief I2S3 Initialization Function
-  * @param None
-  * @retval None
+  /* USER CODE END DAC_Init 1 */
+  /** DAC Initialization
   */
-static void MX_I2S3_Init(void)
-{
-
-  /* USER CODE BEGIN I2S3_Init 0 */
-
-  /* USER CODE END I2S3_Init 0 */
-
-  /* USER CODE BEGIN I2S3_Init 1 */
-
-  /* USER CODE END I2S3_Init 1 */
-  hi2s3.Instance = SPI3;
-  hi2s3.Init.Mode = I2S_MODE_MASTER_TX;
-  hi2s3.Init.Standard = I2S_STANDARD_PHILIPS;
-  hi2s3.Init.DataFormat = I2S_DATAFORMAT_16B;
-  hi2s3.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
-  hi2s3.Init.AudioFreq = I2S_AUDIOFREQ_96K;
-  hi2s3.Init.CPOL = I2S_CPOL_LOW;
-  hi2s3.Init.ClockSource = I2S_CLOCK_PLL;
-  hi2s3.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
-  if (HAL_I2S_Init(&hi2s3) != HAL_OK)
+  hdac.Instance = DAC;
+  if (HAL_DAC_Init(&hdac) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2S3_Init 2 */
+  /** DAC channel OUT1 config
+  */
+  sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
+  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN DAC_Init 2 */
 
-  /* USER CODE END I2S3_Init 2 */
+  /* USER CODE END DAC_Init 2 */
 
 }
 
@@ -334,6 +462,51 @@ static void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 104;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
 
 }
 
@@ -377,6 +550,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DMA_GPIO_Port, DMA_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
                           |Audio_RST_Pin, GPIO_PIN_RESET);
 
@@ -408,6 +584,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : DMA_Pin */
+  GPIO_InitStruct.Pin = DMA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DMA_GPIO_Port, &GPIO_InitStruct);
+
   /*Configure GPIO pin : BOOT1_Pin */
   GPIO_InitStruct.Pin = BOOT1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
@@ -431,11 +614,41 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : I2S3_MCK_Pin I2S3_SCK_Pin I2S3_SD_Pin */
+  GPIO_InitStruct.Pin = I2S3_MCK_Pin|I2S3_SCK_Pin|I2S3_SD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF6_SPI3;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : VBUS_FS_Pin */
+  GPIO_InitStruct.Pin = VBUS_FS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(VBUS_FS_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : OTG_FS_ID_Pin OTG_FS_DM_Pin OTG_FS_DP_Pin */
+  GPIO_InitStruct.Pin = OTG_FS_ID_Pin|OTG_FS_DM_Pin|OTG_FS_DP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : OTG_FS_OverCurrent_Pin */
   GPIO_InitStruct.Pin = OTG_FS_OverCurrent_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(OTG_FS_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : Audio_SCL_Pin Audio_SDA_Pin */
+  GPIO_InitStruct.Pin = Audio_SCL_Pin|Audio_SDA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pin : MEMS_INT2_Pin */
   GPIO_InitStruct.Pin = MEMS_INT2_Pin;
